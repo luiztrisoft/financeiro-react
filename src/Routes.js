e391ffae091df3e52913eb1dce982eec2ab652de
user <luiz.trisoft@gmail.com>
2021-07-10 13:57:30 -0300

    Pagina NaoEncontrado

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
 import Cadastro from "./pages/cadastro/Cadastro.jsx";
 import Consulta from "./pages/consulta/Consulta.jsx";
+import NaoEncontrado from "./pages/NaoEncontrado.jsx";
 
 const Routes = () => (
 	<div className="layout-main">
 		<Route path="/" exact component={Dashboard} />
 		<Route path="/cadastro" exact component={Cadastro} />
 		<Route path="/consulta" exact component={Consulta} />
+		<Route component={NaoEncontrado} />
 	</div>
 );
 export default Routes;
