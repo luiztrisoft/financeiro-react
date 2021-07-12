f8eb8407586adf7f85c704bb7b7cb2a4360ea377
user <luiz.trisoft@gmail.com>
2021-07-12 00:12:05 -0300

    NaoEncontrado

--- src/pages/NaoEncontrado.jsx
+++ src/pages/NaoEncontrado.jsx
@@ -13,8 +13,8 @@ const NaoEncontrado = () => {
         <h1 style={{
             color: '#09f',
             fontWeight: 900
-        }}>404</h1>
-        <p>Não encontrado</p>
+        }}>Ops :(</h1>
+        <p>A página que você está procurando não foi encontrada.</p>
         <Link to="/" style={{
             color: '#09f',
             textDecoration: 'none',
